3dbb93e3508730ab3ffa56d0bf18faac65d26d2c
Alex Marz <alexhmarz@gmail.com>
2020-12-08 19:31:19 -0500

    commit tree 9acc0572cbb0cd5aa385562b3218092e3050a64d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
-  def count_sentences
+  def count_sentences #this required SO MUCH exploration of Regex and I need to review it more. 
+    if self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count > 0
+      return self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+    else
+      sentence_array = self.split.count
+    end
+   
 
   end
 end
